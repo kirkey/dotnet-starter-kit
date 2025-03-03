f5d73629d27f3cc8505b2b65bf60950ffb3c6779
kirkeypsalms <kirkey_psalms@yahoo.com.ph>
2025-03-03 09:02:24 +0800

    added mysql as preparation

--- src/api/framework/Infrastructure/Persistence/DbProviders.cs
+++ src/api/framework/Infrastructure/Persistence/DbProviders.cs
@@ -3,4 +3,5 @@ internal static class DbProviders
 {
     public const string PostgreSQL = "POSTGRESQL";
     public const string MSSQL = "MSSQL";
+    public const string MySql = "MYSQL";
 }
---
+++ src/api/migrations/MySQL/Todo/20240802061357_Add Todo Schema.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSH.Starter.WebApi.Migrations.MySQL.Todo
+{
+    /// <inheritdoc />
+    public partial class AddTodoSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "todo");
+
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                schema: "todo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Note = table.Column<string>(type: "varchar(1000)", maxLength: 1000, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TenantId = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Created = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    LastModified = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    LastModifiedBy = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos",
+                schema: "todo");
+        }
+    }
+}
