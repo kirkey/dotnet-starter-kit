cb52996b4921b76d2458cd8081be7d3349138b61
kirkeypsalms <kirkey_psalms@yahoo.com.ph>
2025-05-08 08:24:18 +0800

    Update GroupMetrics.cs

--- src/api/modules/App/App/Domain/GroupMetrics.cs
+++ src/api/modules/App/App/Domain/GroupMetrics.cs
@@ -6,7 +6,7 @@ namespace FSH.Starter.WebApi.App.Domain;
 public static class GroupMetrics
 {
     private static readonly Meter Meter = new Meter(MetricsConstants.App, "1.0.0");
-    public static readonly Counter<int> Created = Meter.CreateCounter<int>("groups.created");
-    public static readonly Counter<int> Updated = Meter.CreateCounter<int>("groups.updated");
-    public static readonly Counter<int> Deleted = Meter.CreateCounter<int>("groups.deleted");
+    public static readonly Counter<int> Created = Meter.CreateCounter<int>("group.created");
+    public static readonly Counter<int> Updated = Meter.CreateCounter<int>("group.updated");
+    public static readonly Counter<int> Deleted = Meter.CreateCounter<int>("group.deleted");
 }
