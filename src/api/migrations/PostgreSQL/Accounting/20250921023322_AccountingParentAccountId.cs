e971cb9ca312732537a10ef53c7b8076ebced77f
kirkeypsalms <kirkey_psalms@yahoo.com.ph>
2025-09-21 14:32:36 +0800

    feat: Add migrations for AccountingPeriodName and rename SubAccountOf to ParentAccountId in ChartOfAccounts

---
+++ src/api/migrations/PostgreSQL/Accounting/20250921023322_AccountingParentAccountId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSH.Starter.WebApi.Migrations.PostgreSQL.Accounting
+{
+    /// <inheritdoc />
+    public partial class AccountingParentAccountId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubAccountOf",
+                schema: "accounting",
+                table: "ChartOfAccounts",
+                newName: "ParentAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ParentAccountId",
+                schema: "accounting",
+                table: "ChartOfAccounts",
+                newName: "SubAccountOf");
+        }
+    }
+}
---
+++ src/api/migrations/PostgreSQL/Accounting/20250921055629_AccountingPeriodName.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FSH.Starter.WebApi.Migrations.PostgreSQL.Accounting
+{
+    /// <inheritdoc />
+    public partial class AccountingPeriodName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PeriodName",
+                schema: "accounting",
+                table: "Budgets",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PeriodName",
+                schema: "accounting",
+                table: "Budgets");
+        }
+    }
+}
